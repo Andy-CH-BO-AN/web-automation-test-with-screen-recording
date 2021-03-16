984194d385fc5ca54526977db82b29f6c2e4bcc5
Andy-CH-BO-AN <58759761+Andy-CH-BO-AN@users.noreply.github.com>
2021-03-16 15:33:52 +0800

    Add files via upload

---
+++ open_browser.py
@@ -0,0 +1,41 @@
+import os
+import time
+
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+
+
+def open_browser(url):
+    # 跳過驅動器
+    chrome_path = ChromeDriverManager().install()
+    options = webdriver.ChromeOptions()
+    # 禁用瀏覽器彈窗
+    prefs = {
+        'profile.default_content_setting_values':
+            {
+                'notifications': 2
+            }
+    }
+    options.add_experimental_option('prefs', prefs)
+    # 設定瀏覽器分辨率(配合無頭模式)
+    options.add_argument('window-size=1920x1080')
+    # 開啟無頭模式(自行決定是否啟動)
+    # options.add_argument('--headless')
+    # 去除自動化控制提示
+    options.add_argument('--disable-notifications --disable-popup-blocking')
+    # 開啟無痕模式
+    options.add_argument('--incognito')
+    # 文檔提到需要加上這個屬性來規避bug
+    options.add_argument('--disable-gpu')
+    # 設置開發者模式啟動，該模式下webdriver屬性為正常值
+    options.add_experimental_option('excludeSwitches', ['enable-automation'])
+    driver = webdriver.Chrome(chrome_path, options=options)
+    driver.get(url)
+    time.sleep(5)
+    driver.quit()
+
+
+if __name__ == '__main__':
+    url = 'https://www.google.com/'
+    open_browser(url)
+    os.system(r'taskkill /f /im python.exe')
